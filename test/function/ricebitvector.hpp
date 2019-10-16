9d57703db81c33b44c52dbeceb537787852be094
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2019-10-16 17:10:55 +0200

    Fixed

--- test/function/ricebitvector.hpp
+++ test/function/ricebitvector.hpp
@@ -24,25 +24,25 @@ static vector<uint64_t> gen_keys() {
 }
 
 template <sux::util::AllocType AT = sux::util::AllocType::MALLOC>
-static RiceBitVector<AT> build_bitvector(const vector<uint64_t> &keys, const int golomb_param) {
-	auto r = RiceBitVector<AT>();
+static RiceBitVector<AT> *build_bitvector(const vector<uint64_t> &keys, const int golomb_param) {
+	RiceBitVector<AT> *r = new RiceBitVector<AT>();
 	for (size_t t = 0; t < rice_test_ntrees; ++t) {
 		vector<uint32_t> unary;
 		for (uint64_t k : keys) {
-			r.appendFixed(k, golomb_param);
+			r->appendFixed(k, golomb_param);
 			unary.push_back(k >> golomb_param);
 		}
-		r.appendUnaryAll(unary);
+		r->appendUnaryAll(unary);
 	}
 	return r;
 }
 
 template <sux::util::AllocType AT = sux::util::AllocType::MALLOC>
-static void test_rice_trees(RiceBitVector<AT> r, const vector<uint64_t> &keys, const int golomb_param, const size_t tree_offset) {
+static void test_rice_trees(RiceBitVector<AT> *r, const vector<uint64_t> &keys, const int golomb_param, const size_t tree_offset) {
 	for (size_t t = 0; t < rice_test_ntrees; ++t) {
-		r.readReset(t * tree_offset, golomb_param * keys.size());
+		r->readReset(t * tree_offset, golomb_param * keys.size());
 		for (size_t i = 0; i < keys.size(); ++i) {
-			auto k = r.readNext(golomb_param);
+			auto k = r->readNext(golomb_param);
 			ASSERT_EQ(k, keys[i]) << "ERROR: " << k << " != " << keys[i] << endl;
 		}
 	}
@@ -52,89 +52,96 @@ TEST(RiceBitVector_test, trees_golomb_0) {
 	const size_t golomb_param = 0;
 	vector<uint64_t> keys = gen_keys();
 
-	auto r = build_bitvector<sux::util::AllocType::TRANSHUGEPAGE>(keys, golomb_param);
+	auto *r = build_bitvector<sux::util::AllocType::TRANSHUGEPAGE>(keys, golomb_param);
 	size_t tree_offset = 0;
 	for (uint64_t k : keys) {
 		tree_offset += 1 + (k >> golomb_param) + golomb_param;
 	}
 
 	test_rice_trees(r, keys, golomb_param, tree_offset);
+	delete r;
 }
 
 TEST(RiceBitVector_test, trees_golomb_1) {
 	const size_t golomb_param = 1;
 	vector<uint64_t> keys = gen_keys();
 
-	auto r = build_bitvector(keys, golomb_param);
+	auto *r = build_bitvector(keys, golomb_param);
 	size_t tree_offset = 0;
 	for (uint64_t k : keys) {
 		tree_offset += 1 + (k >> golomb_param) + golomb_param;
 	}
 
 	test_rice_trees(r, keys, golomb_param, tree_offset);
+	delete r;
 }
 
 TEST(RiceBitVector_test, trees_golomb_2) {
 	const size_t golomb_param = 2;
 	vector<uint64_t> keys = gen_keys();
 
-	auto r = build_bitvector(keys, golomb_param);
+	auto *r = build_bitvector(keys, golomb_param);
 	size_t tree_offset = 0;
 	for (uint64_t k : keys) {
 		tree_offset += 1 + (k >> golomb_param) + golomb_param;
 	}
 
 	test_rice_trees(r, keys, golomb_param, tree_offset);
+	delete r;
 }
 
 TEST(RiceBitVector_test, trees_golomb_3) {
 	const size_t golomb_param = 3;
 	vector<uint64_t> keys = gen_keys();
 
-	auto r = build_bitvector(keys, golomb_param);
+	auto *r = build_bitvector(keys, golomb_param);
 	size_t tree_offset = 0;
 	for (uint64_t k : keys) {
 		tree_offset += 1 + (k >> golomb_param) + golomb_param;
 	}
 
 	test_rice_trees(r, keys, golomb_param, tree_offset);
+	delete r;
 }
 
 TEST(RiceBitVector_test, trees_golomb_4) {
 	const size_t golomb_param = 4;
 	vector<uint64_t> keys = gen_keys();
 
-	auto r = build_bitvector(keys, golomb_param);
+	auto *r = build_bitvector(keys, golomb_param);
 	size_t tree_offset = 0;
 	for (uint64_t k : keys) {
 		tree_offset += 1 + (k >> golomb_param) + golomb_param;
 	}
 
 	test_rice_trees(r, keys, golomb_param, tree_offset);
+	delete r;
 }
 
 TEST(RiceBitVector_test, trees_golomb_5) {
 	const size_t golomb_param = 5;
 	vector<uint64_t> keys = gen_keys();
 
-	auto r = build_bitvector(keys, golomb_param);
+	auto *r = build_bitvector(keys, golomb_param);
 	size_t tree_offset = 0;
 	for (uint64_t k : keys) {
 		tree_offset += 1 + (k >> golomb_param) + golomb_param;
 	}
 
 	test_rice_trees(r, keys, golomb_param, tree_offset);
+	delete r;
 }
 
 TEST(RiceBitVector_test, trees_golomb_6) {
 	const size_t golomb_param = 6;
 	vector<uint64_t> keys = gen_keys();
 
-	auto r = build_bitvector(keys, golomb_param);
+	auto *r = build_bitvector(keys, golomb_param);
 	size_t tree_offset = 0;
 	for (uint64_t k : keys) {
 		tree_offset += 1 + (k >> golomb_param) + golomb_param;
 	}
 
 	test_rice_trees(r, keys, golomb_param, tree_offset);
+	delete r;
 }
